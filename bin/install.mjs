4ee1250f2c8fd276fc849ac025c8a37b3769e97b
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-07-08 19:17:23 +0700

    fix bugs

--- bin/install.mjs
+++ bin/install.mjs
@@ -11,10 +11,10 @@ const exec = promisify(cp.exec);
 const rm = promisify(fs.rm);
 
 if (process.argv.length < 3) {
-  console.log(chalk.blueBright("Kamu harus menamai projek anda!"));
-  console.log("Contohnya :");
-  console.log("   npx create-express-stack my-server");
-  process.exit(1);
+   console.log(chalk.blueBright("Kamu harus menamai projek anda!"));
+   console.log("Contohnya :");
+   console.log("   npx create-express-stack my-server");
+   process.exit(1);
 }
 
 const projectName = process.argv[2];
@@ -24,55 +24,67 @@ const projectPath = path.join(currentPath, projectName);
 const git_repo = "https://github.com/Rauliqbal/create-express-stack.git";
 
 if (fs.existsSync(projectPath)) {
-  console.log(
-    `Projek ${chalk.green(
-      projectName
-    )} sudah ada di direktori saat ini, harap beri nama lain🙏🏻.`
-  );
+   console.log(
+      `Projek ${chalk.green(
+         projectName
+      )} sudah ada di direktori saat ini, harap beri nama lain🙏🏻.`
+   );
 
-  process.exit(1);
+   process.exit(1);
 } else {
-  fs.mkdirSync(projectPath);
+   fs.mkdirSync(projectPath);
 }
 
 try {
-  const gitSpinner = ora("Downloading files🚀...").start();
-  // clone the repo into the project folder -> creates the new boilerplate
-  await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
-  gitSpinner.succeed();
+   const gitSpinner = ora("Downloading files🚀...").start();
+   // clone the repo into the project folder -> creates the new boilerplate
+   await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
+   gitSpinner.succeed();
 
-  const cleanSpinner = ora("Tunggu sebentar yaa😁...").start();
-  // remove my git history
-  const rmGit = rm(path.join(projectPath, ".git"), {
-    recursive: true,
-    force: true,
-  });
-  // remove the installation file
-  const rmBin = rm(path.join(projectPath, "bin"), {
-    recursive: true,
-    force: true,
-  });
-  await Promise.all([rmGit, rmBin]);
+   const cleanSpinner = ora("Tunggu sebentar yaa😁...").start();
+   // remove my git history
+   const rmGit = rm(path.join(projectPath, ".git"), {
+      recursive: true,
+      force: true,
+   });
+   // remove the installation file
+   const rmBin = rm(path.join(projectPath, "bin"), {
+      recursive: true,
+      force: true,
+   });
+   await Promise.all([rmGit, rmBin]);
 
-  process.chdir(projectPath);
-  // remove the packages needed for cli
-  await exec("npm uninstall ora cli-spinners");
-  cleanSpinner.succeed();
+   process.chdir(projectPath);
+   // remove the packages needed for cli
+   await exec("npm uninstall ora cli-spinners");
+   cleanSpinner.succeed();
 
-  const npmSpinner = ora("Installing dependencies...").start();
-  await exec("npm install");
-  npmSpinner.succeed();
+   const npmSpinner = ora("Installing dependencies...").start();
+   await exec("npm install");
+   npmSpinner.succeed();
 
-  console.log("   ");
-  console.log("Yeay, projek Express Stackmu sudah siap🥳");
-  console.log("Kamu bisa menjalankan projekmu dengan:");
-  console.log(`    cd ${projectName}`);
-  console.log(`    npm run dev`);
-  console.log("   ");
-  console.log("Happy Coding!👾");
-  console.log("   ");
+   console.log(`
+    ████████████████████████
+    ████████████████████████
+    ████████████████████████
+    ████████████████████████
+    ████████████████████████
+    ██████████  ████    ████
+    ██████████  ██  ████████
+    ██████████  ████    ████
+    ██████████  ████████  ██
+    ██████████  ██  ████  ██
+    ██████    ██████    ████
+    ████████████████████████`);
+   console.log("   ");
+   console.log("Yeay, projek Express Stackmu sudah siap🥳");
+   console.log("Kamu bisa menjalankan projekmu dengan:");
+   console.log(`    cd ${projectName}`);
+   console.log(`    npm run dev`);
+   console.log("   ");
+   console.log("Happy Coding!👾");
 } catch (error) {
-  // clean up in case of error, so the user does not have to do it manually
-  fs.rmSync(projectPath, { recursive: true, force: true });
-  console.log(error);
+   // clean up in case of error, so the user does not have to do it manually
+   fs.rmSync(projectPath, { recursive: true, force: true });
+   console.log(error);
 }
