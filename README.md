a8fc4f64c22fb30538fae628b97182374a2adb0f
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-08-24 08:26:31 +0700

    update README

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # EXPRESS STACK - Node Server Boilerplate
 
-[![GitHub version](https://badge.fury.io/gh/Rauliqbal%2Fcreate-express-stack.git.svg)](https://badge.fury.io/gh/Rauliqbal%2Fcreate-express-stack.git)
-
 Boilerplate/starter project to easily create RESTful APIs using Node.js, Express, and Mongoose.
 Simply create a new Express application with one command
 
 ## Quick Started
 
-### Create project
+### Create Server
 
 ```bash
-npx create-express-stack <project-name>
-cd <project-name>
+npx create-express-stack
 ```
 
-### Running Server
-
+### Run Server
 ```bash
 npm run dev
+```
+
+### Deploy 
+```bash
+npm run deploy
 ```
 
 and now you can development application
@@ -25,22 +26,9 @@ and now you can development application
 ## Features
 
 - **Web Framework** : using [Express JS](https://expressjs.com/)
-- **Database No SQL** : [Mongo DB](https://www.mongodb.com/) with object data modeling [Mongoose](https://mongoosejs.com/)
+- **Support ORM & ODM** : [Prisma ORM](https://www.prisma.io/), [Mongoose ODM](https://mongoosejs.com/)
 - **Logging** : using [Morgan](https://www.npmjs.com/package/morgan)
 - **CORS** : Cross-origin resource sharing using [CORS](https://www.npmjs.com/package/cors)
 - **Environment variables** : using [Dotenv](https://www.npmjs.com/package/dotenv)
 - Authentication and Authorization with **jsonwebtoken** [JWT](https://www.npmjs.com/package/jsonwebtoken)
 
-# Project Structure
-
-```
-config\         # Configuration & Database function
-src\
- |--controllers\    # Controllers function
- |--middlewares\    # Custom express middlewares
- |--models\         # Mongoose models
- |--router\         # Routes
- |--docs\           # Swagger files
-.env            # Environment variables
-index.js        # App entry point
-```
--- bin/install.mjs
+++ bin/install.mjs
@@ -5,104 +5,121 @@ import path from "path";
 import fs, { existsSync, mkdirSync } from "fs";
 import ora from "ora";
 import chalk from "chalk";
+import inquirer from "inquirer";
+import { downloadTemplate } from "giget";
 
 // convert libs to promises
 const exec = promisify(cp.exec);
 const rm = promisify(fs.rm);
 
-if (process.argv.length < 3) {
-   console.log("Kamu harus menamai projek anda!");
-   console.log("Contohnya :");
-   console.log(chalk.green.bold("   npx create-express-stack my-server"));
-   process.exit(1);
-}
+// if (process.argv.length < 3) {
+//    console.log("Kamu harus menamai projek anda!");
+//    console.log("Contohnya :");
+//    console.log(chalk.green.bold("   npx create-express-stack my-server"));
+//    process.exit(1);
+// }
 
-const projectName = process.argv[2];
-const currentPath = process.cwd();
-const projectPath = path.join(currentPath, projectName);
+const templates = ["prisma", "mongoose"];
+const dirName = templates;
+const config = {
+  directory: dirName,
+  repository: "starter",
+  user: "AzuraCoder",
+  ref: "main",
+};
+const question = [
+  {
+    name: "project-name",
+    type: "input",
+    message: "What is your name?",
+    default: "my-server",
+  },
+  {
+    name: "project-template",
+    type: "list",
+    message: "Which template do you want to use?",
+    choices: templates,
+  },
+];
+
+//  const projectName = process.argv[2];
+// const projectPath = path.join(currentPath, projectName);
+
 // change to your boilerplate repo
 const git_repo = "https://github.com/Rauliqbal/create-express-stack.git";
+const mongoose = "https://github.com/AzuraCoder/mongoose-template.git"
 
-if (fs.existsSync(projectPath)) {
-   console.log(
+inquirer.prompt(question).then(async (answers) => {
+  const projectName = answers["project-name"];
+  const projectTemplate = answers["project-template"];
+  const currentPath = process.cwd();
+  const projectPath = path.join(currentPath, projectName);
+
+  
+
+  if (fs.existsSync(projectPath)) {
+    console.log(
       `Projek ${chalk.green(
-         projectName
+        projectName
       )} sudah ada di direktori saat ini, harap beri nama lain🙏🏻.`
-   );
+    );
 
-   process.exit(1);
-} else {
-   fs.mkdirSync(projectPath);
-}
+    process.exit(1);
+  } else {
+    fs.mkdirSync(projectPath);
+  }
 
-try {
-   const gitSpinner = ora("Downloading files🚀...").start();
-   // clone the repo into the project folder -> creates the new boilerplate
-   await exec(`git clone --depth 1 ${git_repo} ${projectPath} --quiet`);
-   gitSpinner.succeed();
+  try {
+    const gitSpinner = ora("Downloading files🚀...").start();
+    await exec(`git clone --depth 1 https://github.com/AzuraCoder/${projectTemplate}-template.git  ${projectPath} --quiet`);
+    gitSpinner.succeed();
 
-   const cleanSpinner = ora("Tunggu sebentar yaa😁...").start();
+    const cleanSpinner = ora("Tunggu sebentar yaa😁...").start();
 
-   // Copy environment variables
-   fs.copyFileSync(path.join(projectPath, '.env.example'), path.join(projectPath, '.env'));
+    // Copy environment variables
+    // fs.copyFileSync(path.join(projectPath, '.env.example'), path.join(projectPath, '.env'));
 
-
-   // remove my git history
-   const rmGit = rm(path.join(projectPath, ".git"), {
+    // remove my git history
+    const rmGit = rm(path.join(projectPath, ".git"), {
       recursive: true,
       force: true,
-   });
-   // remove the installation file
-   const rmBin = rm(path.join(projectPath, "bin"), {
-      recursive: true,
-      force: true,
-   });
-   // remove license file
-   const rmLicense = rm(path.join(projectPath, "LICENSE"), {
-      recursive: true,
-      force: true,
-   });
-   // remove license file
-   const rmEnv = rm(path.join(projectPath, ".env.example"), {
-      recursive: true,
-      force: true,
-   });
+    });
+    // // remove the installation file
+    // const rmBin = rm(path.join(projectPath, "bin"), {
+    //    recursive: true,
+    //    force: true,
+    // });
+    // // remove license file
+    // const rmLicense = rm(path.join(projectPath, "LICENSE"), {
+    //    recursive: true,
+    //    force: true,
+    // });
+    // // remove env file
+    // const rmEnv = rm(path.join(projectPath, ".env.example"), {
+    //    recursive: true,
+    //    force: true,
+    // });
 
+    await Promise.all([rmGit]);
 
+    process.chdir(projectPath);
+    // remove the packages needed for cli
+    await exec("npm uninstall ora cli-spinners");
+    cleanSpinner.succeed();
 
-   await Promise.all([rmGit, rmBin, rmLicense, rmEnv]);
+    const npmSpinner = ora("Installing dependencies...").start();
+    await exec("npm install");
+    npmSpinner.succeed();
 
-   process.chdir(projectPath);
-   // remove the packages needed for cli
-   await exec("npm uninstall ora cli-spinners");
-   cleanSpinner.succeed();
-
-   const npmSpinner = ora("Installing dependencies...").start();
-   await exec("npm install");
-   npmSpinner.succeed();
-
-   console.info(chalk.yellow(`
-    ████████████████████████
-    ████████████████████████
-    ████████████████████████
-    ████████████████████████
-    ████████████████████████
-    ██████████  ████    ████
-    ██████████  ██  ████████
-    ██████████  ████    ████
-    ██████████  ████████  ██
-    ██████████  ██  ████  ██
-    ██████    ██████    ████
-    ████████████████████████`));
-   console.log("   ");
-   console.log("Yeay, projek Express Stackmu sudah siap🥳");
-   console.log("Kamu bisa menjalankan projekmu dengan:");
-   console.log(chalk.green.bold(`    cd ${projectName}`));
-   console.log(chalk.green.bold(`    npm run dev`));
-   console.log("   ");
-   console.log(chalk.bgBlue.bold(" Happy Coding!👾 "));
-} catch (error) {
-   // clean up in case of error, so the user does not have to do it manually
-   fs.rmSync(projectPath, { recursive: true, force: true });
-   console.log(error);
-}
+    console.log("🎉Yeay, projek Express Stackmu sudah siap🥳");
+    console.log(chalk.gray("Get started with:"));
+    console.log(chalk.green.bold(`    cd ${projectName}`));
+    console.log(chalk.green.bold(`    npm run dev`));
+    console.log("   ");
+    console.log(chalk.blue.bold(" Happy Coding!👾 "));
+  } catch (error) {
+    // clean up in case of error, so the user does not have to do it manually
+    fs.rmSync(projectPath, { recursive: true, force: true });
+    console.log(error);
+  }
+});
